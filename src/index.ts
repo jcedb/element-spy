7c4df67b2dd023da7f419ea4b4aecdeccc00b329
Ced <jcedb07@gmail.com>
2022-03-10 15:27:12 +0800

    tsdx usage

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+type TTarget = string | HTMLElement | Element | any;
+type TListener = 'DOMContentLoaded' | 'load' | 'scroll' | 'resize';
+
+const getElement = (target: TTarget) => {
+  const isString = typeof target === 'string';
+
+  return isString ? document.querySelector(target) : target;
+};
+
+const isInViewport = (rect: DOMRect, callback: () => void) => {
+  let { top, left, width, height } = rect;
+
+  // while (el.offsetParent) {
+  //   el = el.offsetParent as HTMLElement;
+  //   top += el.offsetTop;
+  //   left += el.offsetLeft;
+  // }
+
+  if (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    top + height <= window.pageYOffset + window.innerHeight &&
+    left + width <= window.pageXOffset + window.innerWidth
+  )
+    callback();
+};
+
+export const elementSpy = (
+  target: TTarget,
+  callback: () => void,
+  listener: TListener | TListener[] = [
+    'DOMContentLoaded',
+    'load',
+    'scroll',
+    'resize',
+  ]
+) => {
+  window.onload = () => {
+    const el = getElement(target) as HTMLElement;
+    const rect = el.getBoundingClientRect();
+
+    if (typeof listener === 'string') {
+      addEventListener(listener, () => isInViewport(rect, callback), false);
+    } else if (Array.isArray(listener)) {
+      for (let i = 0; i < listener.length; i++) {
+        addEventListener(
+          listener[i],
+          () => isInViewport(rect, callback),
+          false
+        );
+      }
+    }
+  };
+};
